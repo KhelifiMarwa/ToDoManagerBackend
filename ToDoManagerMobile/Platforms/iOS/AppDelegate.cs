b91bab6d2948e0e8d6cbbd75cb65a16618d17f0a
Marwa KHELIFI <Marwa.KHELIFI@polytechnicien.tn>
2025-09-08 09:24:19 +0200

    Add mobile app project to solution

---
+++ ToDoManagerMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ToDoManagerMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
